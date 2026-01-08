8455d23bc47fac94b3fd2104fb5b5851c8360283
Ruthless Creature <georgijxe@gmail.com>
2026-01-08 16:06:19 +0800

    Create everyday.bat
    新增一键更新脚本

---
+++ everyday.bat
@@ -0,0 +1,10 @@
+@echo off
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value') do set datetime=%%i
+set today=%datetime:~0,8%
+
+call conda activate p312
+
+python astock_analyzer.py run --start-date %today% --end-date %today% --workers 16
+python astock_analyzer.py export --output output/pool_%today%.csv --top 200 --min-score 70 --require-tags TREND_UP,AT_SUPPORT
+
+pause
